d91a57a0807cd82fbd96341de122fae26154f6c8
Veselin Ivanov <me@veselyn.com>
2024-09-09 17:36:51 +0200

    feat(flake): initialize flake-parts

--- flake.nix
+++ flake.nix
@@ -2,8 +2,25 @@
   description = "universe";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts/main";
   };
 
-  outputs = inputs: {
-  };
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+      ];
+      systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
+      perSystem = {
+        config,
+        inputs',
+        pkgs,
+        self',
+        system,
+        ...
+      }: {
+      };
+      flake = {
+      };
+    };
 }
